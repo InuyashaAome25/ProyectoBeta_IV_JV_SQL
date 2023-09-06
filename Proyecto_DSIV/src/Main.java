f1e6d7d2fa75de3c892763217921a02f78b648df
InuyashaAome25 <eliascastillo2509@gmail.com>
2023-09-05 22:06:47 -0500

    Se agrega nuevas clases

--- Proyecto_DSIV/src/Main.java
+++ Proyecto_DSIV/src/Main.java
@@ -1,5 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
     }
 }
